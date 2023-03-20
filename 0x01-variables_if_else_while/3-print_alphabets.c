36912014e00168470104d228a169a822a01543b1
Baffana <baffanatesfaye27@gmail.com>
2023-03-20 12:08:31 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 
-char alp[52] = "abcdefghijklmnopqrstuvwqyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	for (i = 0; i < 52; i++)
@@ -18,4 +18,4 @@ char alp[52] = "abcdefghijklmnopqrstuvwqyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	}
 	putchar('\n');
 	return (0);
-	}
+}
