5376c310a86eb60b795a3a380628332043423079
Baffana <baffanatesfaye27@gmail.com>
2023-03-20 12:19:09 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,7 +4,6 @@
  * main - Entry point
  *
  * Description: 'the program's description'
- * @parameter: describe the parameter
  *
  * Return: Always 0 (Success)
  */
@@ -15,7 +14,7 @@ char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	for (i = 0; i < 52; i++)
-	{	
+	{
 		putchar(alp[i]);
 	}
 	putchar('\n');
